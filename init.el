bdaa0ef576c78deece3e9059d338908e7c975c6b
test <test>
2020-09-06 18:04:47 -0400

    removed yas-classic-snippets

--- init.el
+++ init.el
@@ -58,9 +58,6 @@
 (unless (package-installed-p 'yasnippet)
   (package-refresh-contents)
   (package-install 'yasnippet))
-(unless (package-installed-p 'yasnippet-classic-snippets)
-  (package-refresh-contents)
-  (package-install 'yasnippet-classic-snippets))
 (unless (package-installed-p 'yasnippet-snippets)
   (package-refresh-contents)
   (package-install 'yasnippet-snippets))
@@ -151,3 +148,16 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(ido-vertical-mode avy smex beacon go-snippets go go-mode yasnippet-snippets yasnippet-classic-snippets yasnippet which-key dashboard smartparens evil use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
