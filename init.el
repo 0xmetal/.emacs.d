065579abeca89a426082783c89ec2faaeaafaf72
test <test>
2020-09-06 21:38:35 -0400

    emacs is clean

--- init.el
+++ init.el
@@ -1,5 +1,4 @@
 ;; Optimizations
-;;
 ;; In noninteractive sessions, prioritize non-byte-compiled source files to
 ;; prevent the use of stale byte-code. Otherwise, it saves us a little IO time
 ;; to skip the mtime checks on every *.elc file.
@@ -15,14 +14,11 @@
 (setq gc-cons-threshold (* 1024 1024 100))
 
 ;; Set file-name-handler-alist
-
 ;; Also from https://www.reddit.com/r/emacs/comments/3kqt6e/2_easy_little_known_steps_to_speed_up_emacs_start/
-
 (setq file-name-handler-alist-original file-name-handler-alist)
 (setq file-name-handler-alist nil)
 
 ;; Set deferred timer to reset them
-
 (run-with-idle-timer
  5 nil
  (lambda ()
