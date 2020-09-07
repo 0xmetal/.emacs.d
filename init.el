e9394e71640599f67d73976ae348be4e7876582e
metlx <69979277+metlx@users.noreply.github.com>
2020-09-06 21:20:08 -0400

    Update init.el

--- init.el
+++ init.el
@@ -165,16 +165,5 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(ido-vertical-mode avy smex beacon go-snippets go go-mode yasnippet-snippets yasnippet-classic-snippets yasnippet which-key dashboard smartparens evil use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+
