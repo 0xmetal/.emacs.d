d7f25840c92df7b4c8c0e6b9e17828c98b63fa69
test <test>
2020-09-07 16:37:40 -0400

    correction

--- init.el
+++ init.el
@@ -168,22 +168,3 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(custom-enabled-themes nil)
- '(package-selected-packages
-   '(rainbow-delimiters-mode color-delimiters-mode global-color-identifiers-mode highlight-parentheses color-identifiers-mode ido-vertical-mode avy smex beacon go-snippets go go-mode yasnippet-snippets yasnippet which-key dashboard smartparens evil use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
