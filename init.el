16ba7dd700ded7c21dcf013e5d8deae42c7a91c3
metlx <metlx>
2022-05-12 01:43:43 -0400

    new config who dis

--- init.el
+++ init.el
@@ -1,8 +1,43 @@
-(setq inhibit-startup-message t)
-(global-linum-mode 1)
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
+;;; init-mini.el --- Centaur Emacs minimal configurations.	-*- lexical-binding: t no-byte-compile: t -*-
+
+;; Copyright (C) 2018-2022 Vincent Zhang
+
+;; Author: Vincent Zhang <seagle0128@gmail.com>
+;; URL: https://github.com/seagle0128/.emacs.d
+;; Version: 1.1.0
+;; Keywords: .emacs.d centaur
+
+;; This file is not part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+;;
+
+;;; Commentary:
+;;
+;; Minimal configurations for debugging purpose.
+;;
+
+;;; Code:
+
+;; Load path
+(push (expand-file-name "site-lisp" user-emacs-directory) load-path)
+(push (expand-file-name "lisp" user-emacs-directory) load-path)
+
+;; Packages
+;; Without this comment Emacs25 adds (package-initialize) here
 
 (require 'package)
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
@@ -13,9 +48,83 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
+(use-package company :ensure t :defer 20
+  ;; This is not perfect yet. It completes too quickly outside programming modes, but while programming it is just right.
+  :custom
+  (company-idle-delay 0.1)
+  (global-company-mode t)
+  (debug-on-error nil) ;; otherwise this throws lots of errors on completion errors
+  :config
+  (define-key company-active-map (kbd "TAB") 'company-complete-selection)
+  (define-key company-active-map (kbd "<tab>") 'company-complete-selection)
+  (define-key company-active-map [return] 'company-complete-selection)
+  (define-key company-active-map (kbd "RET") 'company-complete-selection)
+  ;; auto-complete compatibility
+  (defun my-company-visible-and-explicit-action-p ()
+    (and (company-tooltip-visible-p)
+         (company-explicit-action-p)))
+  (defun company-ac-setup ()
+    "Sets up `company-mode' to behave similarly to `auto-complete-mode'."
+    (setq company-require-match nil)
+    (setq company-auto-complete #'my-company-visible-and-explicit-action-p)
+    (setq company-frontends '(company-echo-metadata-frontend
+                              company-pseudo-tooltip-unless-just-one-frontend-with-delay
+                              company-preview-frontend))
+    (define-key company-active-map [tab]
+      'company-select-next-if-tooltip-visible-or-complete-selection)
+    (define-key company-active-map (kbd "TAB")
+      'company-select-next-if-tooltip-visible-or-complete-selection))
+
+  (company-ac-setup)
+  (add-hook 'js2-mode-hook (lambda () (company-mode))))
+
+;; optional if you want which-key integration
 (use-package which-key
   :ensure t
-  :config (which-key-mode))
+  :config
+  (which-key-mode))
+
+;; if you want to change prefix for lsp-mode keybindings.
+(setq lsp-keymap-prefix "s-l")
+(use-package lsp-mode
+  :ensure t
+  :init
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+
+  ;; if you want to change prefix for lsp-mode keybindings.
+  (setq lsp-keymap-prefix "C-l")
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         (js2-mode . lsp)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
+(add-hook 'js2-mode-hook #'lsp)
+
+(use-package js2-mode :ensure t :defer 20
+  :mode
+  (("\\.js\\'" . js2-mode))
+  :custom
+  (js2-include-node-externs t)
+  (js2-global-externs '("customElements"))
+  (js2-highlight-level 3)
+  (js2r-prefer-let-over-var t)
+  (js2r-prefered-quote-type 2)
+  (js-indent-align-list-continuation t)
+  (global-auto-highlight-symbol-mode t)
+  :config
+  (setq js-indent-level 2)
+  ;; patch in basic private field support
+  (advice-add #'js2-identifier-start-p
+            :after-until
+            (lambda (c) (eq c ?#))))
+
+;; optionally
+(use-package lsp-ui :commands lsp-ui-mode)
+;; if you are helm user
+(use-package helm-lsp :commands helm-lsp-workspace-symbol)
+;; if you are ivy user
+(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
+(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
 
 (use-package magit
   :ensure t
@@ -26,7 +135,7 @@
   :config
   (setq doom-themes-enable-bold t
         doom-themes-enable-italic t)
-  (load-theme 'doom-1337 t))
+  (load-theme 'doom-molokai t))
 
 (use-package rainbow-delimiters
   :ensure t
@@ -35,21 +144,6 @@
 
 (add-hook 'prog-mode-hook 'global-prettify-symbols-mode t)
 
-(setq ring-bell-function 'ignore) 
-(setq make-backup-files nil)
-(setq auto-save-default nil)
-
-(global-set-key (kbd "C-x C-p") 'previous-buffer)
-(global-set-key (kbd "C-x C-n") 'next-buffer)
-
-(progn
-  (setq whitespace-style (quote (face spaces tabs newline space-mark tab-mark newline-mark )))
-  (setq whitespace-display-mappings
-        '(
-          (newline-mark 10 [8629 10]))))
-(add-hook 'prog-mode-hook 'whitespace-newline-mode)
-
-
 (use-package rainbow-identifiers :ensure t
   :config
   (defun rainbow-identifiers--bolden-faces ()
@@ -57,3 +151,150 @@
       (face-remap-add-relative (intern (format "rainbow-identifiers-identifier-%d" (1+ i))) :weight 'bold)))
   (add-hook 'rainbow-identifiers-mode-hook 'rainbow-identifiers--bolden-faces)
   (rainbow-identifiers--bolden-faces))
+
+;; Explicitly set the prefered coding systems to avoid annoying prompt
+;; from emacs (especially on Microsoft Windows)
+(prefer-coding-system 'utf-8)
+
+;; Better defaults
+;; (setq initial-scratch-message nil)
+(global-linum-mode 1)
+(setq ring-bell-function 'ignore) 
+(setq inhibit-splash-screen t)
+(setq uniquify-buffer-name-style 'post-forward-angle-brackets) ; Show path if names are same
+(setq adaptive-fill-regexp "[ t]+|[ t]*([0-9]+.|*+)[ t]*")
+(setq adaptive-fill-first-line-regexp "^* *$")
+(setq delete-by-moving-to-trash t)         ; Deleting files go to OS's trash folder
+(setq make-backup-files nil)               ; Forbide to make backup files
+(setq auto-save-default nil)               ; Disable auto save
+(setq set-mark-command-repeat-pop t)       ; Repeating C-SPC after popping mark pops it again
+;; (setq kill-whole-line t)                   ; Kill line including '\n'
+
+(setq-default major-mode 'text-mode)
+
+(setq sentence-end "\\([。！？]\\|……\\|[.?!][]\"')}]*\\($\\|[ \t]\\)\\)[ \t\n]*")
+(setq sentence-end-double-space nil)
+
+;; Tab and Space
+;; Permanently indent with spaces, never with TABs
+(setq-default c-basic-offset   2
+              tab-width        2
+              indent-tabs-mode nil)
+
+;; UI
+(unless (eq window-system 'ns)
+  (menu-bar-mode -1))
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
+(when (fboundp 'horizontal-scroll-bar-mode)
+  (horizontal-scroll-bar-mode -1))
+
+;; (global-hl-line-mode 1)
+
+;; (if (fboundp 'display-line-numbers-mode)
+;;     (global-display-line-numbers-mode 1)
+;;   (global-linum-mode 1))
+
+;; Basic modes
+(recentf-mode 1)
+(ignore-errors (savehist-mode 1))
+(save-place-mode 1)
+(show-paren-mode 1)
+(delete-selection-mode 1)
+(global-auto-revert-mode 1)
+
+(setq electric-pair-inhibit-predicate 'electric-pair-conservative-inhibit)
+(electric-pair-mode 1)
+
+(add-hook 'prog-mode-hook #'subword-mode)
+(add-hook 'minibuffer-setup-hook #'subword-mode)
+
+;; Completion
+(if (fboundp 'fido-mode)
+    (progn
+      (fido-mode 1)
+      (when (fboundp 'fido-vertical-mode)
+        (fido-vertical-mode 1))
+
+      (defun fido-recentf-open ()
+        "Use `completing-read' to find a recent file."
+        (interactive)
+        (if (find-file (completing-read "Find recent file: " recentf-list))
+            (message "Opening file...")
+          (message "Aborting")))
+      (global-set-key (kbd "C-x C-r") 'fido-recentf-open))
+  (progn
+    (ido-mode 1)
+    (ido-everywhere 1)
+
+    (setq ido-use-virtual-buffers t
+          ido-use-filename-at-point 'guess
+          ido-create-new-buffer 'always
+          ido-enable-flex-matching t)
+
+    (defun ido-recentf-open ()
+      "Use `ido-completing-read' to find a recent file."
+      (interactive)
+      (if (find-file (ido-completing-read "Find recent file: " recentf-list))
+          (message "Opening file...")
+        (message "Aborting")))
+    (global-set-key (kbd "C-x C-r") 'ido-recentf-open)))
+
+;; Key Modifiers
+(cond
+ ((eq system-type 'windows-nt)
+  ;; make PC keyboard's Win key or other to type Super or Hyper
+  ;; (setq w32-pass-lwindow-to-system nil)
+  (setq w32-lwindow-modifier 'super     ; Left Windows key
+        w32-apps-modifier 'hyper)       ; Menu/App key
+  (w32-register-hot-key [s-t]))
+ ((eq window-system 'mac)
+  ;; Compatible with Emacs Mac port
+  (setq mac-option-modifier 'meta
+        mac-command-modifier 'super)
+  (global-set-key [(super a)] #'mark-whole-buffer)
+  (global-set-key [(super v)] #'yank)
+  (global-set-key [(super c)] #'kill-ring-save)
+  (global-set-key [(super s)] #'save-buffer)
+  (global-set-key [(super l)] #'goto-line)
+  (global-set-key [(super w)] #'delete-frame)
+  (global-set-key [(super z)] #'undo)))
+
+;; Keybindings
+(global-set-key (kbd "C-.") #'imenu)
+(global-set-key (kbd "<C-return>") #'rectangle-mark-mode)
+
+(defun revert-current-buffer ()
+  "Revert the current buffer."
+  (interactive)
+  (message "Revert this buffer")
+  (text-scale-set 0)
+  (widen)
+  (revert-buffer t t))
+(global-set-key (kbd "<f5>") #'revert-current-buffer)
+
+(add-hook 'emacs-lisp-mode-hook
+          (lambda ()
+            (local-set-key (kbd "C-c C-x") #'ielm)
+            (local-set-key (kbd "C-c C-c") #'eval-defun)
+            (local-set-key (kbd "C-c C-b") #'eval-buffer)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Init-mini.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("cf922a7a5c514fad79c483048257c5d8f242b21987af0db813d3f0b138dfaf53" default))
+ '(package-selected-packages
+   '(which-key lsp-mode rainbow-identifiers rainbow-delimiters doom-themes magit use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#191919" :foreground "#d4d4d4" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 102 :width normal :foundry "outline" :family "Consolas")))))
