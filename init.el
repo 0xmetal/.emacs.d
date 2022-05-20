236a00a76b932dfc6e44ec5e8c6e821eb80ed2fd
metlx <metlx>
2022-05-20 01:54:34 -0400

    need this

--- init.el
+++ init.el
@@ -15,6 +15,13 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
+(use-package undo-fu
+  :ensure t
+  :config
+  (global-unset-key (kbd "C-z"))
+  (global-set-key (kbd "C-z")   'undo-fu-only-undo)
+  (global-set-key (kbd "C-S-z") 'undo-fu-only-redo))
+
 ;; this is pog
 (use-package golden-ratio
   :ensure t
@@ -136,3 +143,16 @@
             (local-set-key (kbd "C-c C-b") #'eval-buffer)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(undo-fu magit js2-mode doom-themes highlight-numbers rainbow-delimiters golden-ratio use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
