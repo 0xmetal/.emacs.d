584dffa8ea5660c15a1f2c240709dcc8f236f63d
Marcos Torres <towers1337@gmail.com>
2022-05-22 20:34:23 -0400

    Update init.el

--- init.el
+++ init.el
@@ -121,16 +121,3 @@
             (local-set-key (kbd "C-c C-b") #'eval-buffer)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(magit js2-mode doom-themes highlight-numbers rainbow-delimiters golden-ratio undo-fu beacon use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
